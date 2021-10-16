f4102afd9ff2df1aec2e5c6b4f63a93234a54c04
boiledcactus <megansimtzeyen@gmail.com>
2021-10-17 06:46:37 +0800

    added datasets, added preprocessing + initial EDA

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -1,49 +1,176 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+libs <- c("sf","tmap","tidyverse","maptools","spatstat","raster",
+          "ggplot2","rgeos","rgdal","sp", "stringr")
+lapply(libs, library, character.only = TRUE)
 
-# Define UI for application that draws a histogram
+###########################################################################################
+
+# assumption that data upload tool is done! 
+# as such, referring to the datasets directly
+# actually can we just use open street map as the base? 
+# but we'd still need the study area boundaries right
+
+# import geospatial data (boundaries of study area)
+# input: shapefile
+# output: simple features object
+
+sg_sf <- st_read(dsn = "data/geospatial", 
+                 layer="CostalOutline")
+mpsz_sf <- st_read(dsn = "data/geospatial", 
+                   layer = "MP14_SUBZONE_WEB_PL")
+shan_sf <- st_read(dsn = "data/geospatial", 
+                   layer = "myanmar_township_boundaries") %>%
+    filter(ST %in% c("Shan (East)", "Shan (North)", "Shan (South)"))
+london_sf <- st_read(dsn = "data/geospatial/statistical-gis-boundaries-london", 
+                     layer = "London_Borough_Excluding_MHW")
+
+# geospatial pre-processing function needs to:
+# - check for invalid geometries
+# - check for missing values
+# - verify + transform CRS
+
+# assumption that user's data upload is stored as input$filename
+
+geospatial_processing <- function(input_sf, expected_crs){
+    return_sf <- input_sf
+    if(length(which(st_is_valid(input_sf) == FALSE)) == 1){
+        return_sf <- st_make_valid(return_sf)
+    }
+    return_sf <- na.omit(return_sf) %>%
+        st_transform(return_sf, crs=expected_crs)
+    return(return_sf)
+}
+
+sg_sf <- geospatial_processing(sg_sf,3414)
+mpsz_sf <- geospatial_processing(mpsz_sf,3414)
+shan_sf <- geospatial_processing(shan_sf,4326)
+london_sf <- geospatial_processing(london_sf,27700)
+
+###########################################################################################
+
+# import aspatial data without lng/lat
+# input: .csv with district names
+# output: tibble data.frame
+
+# assumption that user's data upload is stored as input$filename
+# so to access, read_csv(input$filename$datapath)
+ict <- read_csv ("data/aspatial/Shan-ICT.csv")
+crime <- read_csv ("data/aspatial/crime-types.csv")
+
+# needs to left_join with geospatial data... which is user input? 'join on...'
+# how to add that functionality though :-(
+# or we can necessitate that they joined column must be of same name then go through both to find
+
+shan_ict <- left_join(shan_sf, ict, 
+                     by=c("TS_PCODE"="Township Pcode"))
+
+###########################################################################################
+
+# import aspatial data with lng/lat (transform to geospatial)
+# assumption that all Lng/Lat are ESPG 4326 i.e. WGS84, World Geodetic System 1984
+# input: .csv with lng/lat
+# output: simple features data frame
+
+# pre-processing function needs to:
+# - import data
+# - remove missing values for relevant columns (lat, lng)
+# - convert to sf objects
+# - transform CRS (user input)
+
+aspatial_processing <- function(input_filepath, expected_crs){
+    filetype <- str_sub(input_filepath, -3, -1) 
+    if(filetype=="csv"){
+        return_sf <- read_csv(input_filepath)
+    }else if(filetype=="rds"){
+        return_sf <- read_rds(input_filepath)
+    }
+    
+    return_sf$Lat <- as.numeric(return_sf$Lat)
+    return_sf$Lng <- as.numeric(return_sf$Lng)
+    
+    if(sum(is.na(return_sf$Lat))>0){
+        return_sf <- return_sf[!(is.na(return_sf$Lat)), ]
+    }
+    return_sf <- st_as_sf(return_sf,
+                          coords = c("Lng", "Lat"),
+                          crs = 4326) %>%
+        st_transform(crs=expected_crs)
+    return(return_sf)
+}
+
+listings_2019 <- aspatial_processing("data/aspatial/listings_30062019.csv", 3414)
+listings_2021 <- aspatial_processing("data/aspatial/listings_29062021.csv", 3414)
+childcare_sf <- aspatial_processing("data/aspatial/childcare.rds", 3414)
+
+# deriving new variables - penetration rate
+# need a tool for this?
+ict_derived <- ict %>%
+    mutate(`RADIO_PR` = `Radio`/`Total households`*1000) %>%
+    mutate(`TV_PR` = `Television`/`Total households`*1000) %>%
+    mutate(`LLPHONE_PR` = `Land line phone`/`Total households`*1000) %>%
+    mutate(`MPHONE_PR` = `Mobile phone`/`Total households`*1000) %>%
+    mutate(`COMPUTER_PR` = `Computer`/`Total households`*1000) %>%
+    mutate(`INTERNET_PR` = `Internet at home`/`Total households`*1000) %>%
+    rename(`DT_PCODE` =`District Pcode`,`DT`=`District Name`,
+           `TS_PCODE`=`Township Pcode`, `TS`=`Township Name`,
+           `TT_HOUSEHOLDS`=`Total households`,
+           `RADIO`=`Radio`, `TV`=`Television`, 
+           `LLPHONE`=`Land line phone`, `MPHONE`=`Mobile phone`,
+           `COMPUTER`=`Computer`, `INTERNET`=`Internet at home`) 
+
+###########################################################################################
+
+# EDA SECTION
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("EDA"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(inputId = "room_type",
+                        label = "Which room type?",
+                        choices = c("Private Room" = "Private room",
+                                    "Entire Home/Apartment" = "Entire home/apt",
+                                    "Shared Room" = "Shared room"),
+                        selected = "Private room",
+                        multiple = TRUE),
+            sliderInput(inputId = "price", 
+                        label = "Price", 
+                        min = 0,
+                        max = 13999, 
+                        value = c(100,1000)),
+            checkboxInput(inputId = "showData",
+                          label = "Show data table",
+                          value = TRUE)
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            tmapOutput("mapPlot"),
+            DT::dataTableOutput(outputId = "aTable")
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+server <- function(input, output, session){
+    dataset = reactive({
+        listings_2019 %>%
+            filter(room_type == input$room_type) %>%
+            filter(price > input$price)
     })
+    output$mapPlot <- renderTmap({
+        tm_shape(shp = dataset(),
+                 bbox = st_bbox(listings_2019))+
+            tm_bubbles(col = "room_type",
+                       size = "price",
+                       border.col = "black",
+                       border.lwd = 0.5)
+    })  
+    output$aTable <- DT::renderDataTable({
+        if(input$showData){
+            DT::datatable(data = dataset() %>%
+                              select(1:4),
+                          options= list(pageLength = 10),
+                          rownames = FALSE)
+        }
+    })    
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp (ui=ui, server=server)
---
+++ shiny_app/data/geospatial/statistical-gis-boundaries-london/readme.txt
@@ -0,0 +1,15 @@
+The folder includes:
+
+- Output Area (OA) 2011,
+
+- Lower Super Output Area (LSOA) 2004 and 2011,
+
+- Middle Super Output Area (MSOA) 2004 and 2011,
+
+- London Wards (two files: City of London merged into single area and split into seperate wards). There are separate download file for 2014 & 2018 boundaries.
+
+- London Boroughs
+
+Note: The OA to MSOA boundaries have been generalised to reduce file size/loading time.
+
+On maps created using these boundaries the copyright must be stated. This is: "Contains National Statistics data © Crown copyright and database right [2015]" and "Contains Ordnance Survey data © Crown copyright and database right [2015]"
